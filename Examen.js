b6d28a6b5e068909b769595467598b9096664b7e
Luso233 <luisquintero2001@gmail.com>
2018-09-04 20:34:04 -0500

    Creado mi repo :D

---
+++ Examen.js
@@ -0,0 +1,25 @@
+//Given an input string, reverse the string word by word. For example:
+//given s = "the sky is blue", return "blue is sky the".
+
+Let estring = "";
+for (let i = 0; i < estring.length; i++) {
+    console.log 
+    
+}
+
+//Given a string, find the length of the longest substring without repeating characters. 
+//For example, the longest substring without repeating letters for "abcabcbb" is"abc",
+//which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+
+
+//Given two words, beginWord and endWord, and a wordList of approved words, find the length of the shortest
+//transformation sequence from beginWord to endWord such that:
+//Only one letter can be changed at a time
+//Each transformed word must exist in the wordList.
+//Return the length of the shortest transformation sequence, or 0 if no such transformation sequence exists.
+//Note: beginWord does not count as a transformed word. You can assume that beginWord and endWord are not
+//empty and are not the same.
+//Example:  For beginWord = "hit", endWord = "cog", and wordList = ["hot", "dot", "lot", "log", "cog"],
+//the output should be wordLadder(beginWord, endWord, wordList) = 5.
+//The shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog" with a length of 5
